4f9ad33c61761e6cc9c09758a6f09439ec6c22a2
jaiganesh707 <jaiganeshjkm@gmail.com>
2026-03-04 12:54:13 +0530

    Chnage the URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: '' // Render backend URL will be provided at build time or via proxy
+    apiUrl: 'https://z-invoice.onrender.com'
 };
